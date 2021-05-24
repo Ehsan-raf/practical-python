e660a62165857dd283002e578063b84af0ecb463
Ehsan Rafieiazad <erafieiazad@mun.ca>
2021-05-24 01:41:56 -0230

    Update pcost.py
    pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,30 @@
 # pcost.py
-#
+
+import csv
+def portfolio_cost(filename):
+
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            except ValueError:
+                print('Bad row:', row)
+
+    return total_cost
+
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
 # Exercise 1.27
