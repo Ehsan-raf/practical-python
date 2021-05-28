beb4f4df27c26af0c237082ea80103df0d4f172d
Ehsan Rafieiazad <erafieiazad@mun.ca>
2021-05-28 00:34:31 -0230

    2_16

--- Work/pcost.py
+++ Work/pcost.py
@@ -8,13 +8,14 @@ def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                nshares = int(row[1])
-                price = float(row[2])
+                nshares = int(record['shares'])
+                price = float(record['price'])
                 total_cost += nshares * price
             except ValueError:
-                print('Bad row:', row)
+                print(f'Row {rowno}: Bad row: {row}')
 
     return total_cost
 
@@ -27,4 +28,4 @@ else:
 cost = portfolio_cost(filename)
 print('Total cost:', cost)
 
-# Exercise 1.27
+# Exercise 2_16
