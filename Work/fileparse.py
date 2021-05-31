e774c8ac022b7e548c2bef286adfb578c2d10c1a
Ehsan Rafieiazad <erafieiazad@mun.ca>
2021-05-30 21:42:17 -0230

    3_5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,18 +1,14 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None):
-    '''
-    Parse a CSV file into a list of records
-    '''
+def parse_csv(filename, select=None, types=None):
+
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
+
         headers = next(rows)
 
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
         if select:
             indices = [headers.index(colname) for colname in select]
             headers = select
@@ -20,14 +16,17 @@ def parse_csv(filename, select=None):
             indices = []
 
         records = []
+        
         for row in rows:
-            if not row:    # Skip rows with no data
+            if not row: 
                 continue
-            # Filter the row if specific columns were selected
+                
             if indices:
                 row = [ row[index] for index in indices ]
 
-            # Make a dictionary
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+                
             record = dict(zip(headers, row))
             records.append(record)
 
