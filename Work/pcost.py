633b644a147d48c45fae6c6bd1d855d61a254e43
Ehsan Rafieiazad <erafieiazad@mun.ca>
2021-05-30 21:35:21 -0230

    3_16

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # pcost.py
 
 import report
@@ -7,11 +8,12 @@ def portfolio_cost(filename):
     portfolio = report.read_portfolio(filename)
     return sum([s['shares']*s['price'] for s in portfolio])
 
-import sys
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = input('Enter a filename:')
-
-cost = portfolio_cost(filename)
-print('Total cost:', cost)
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Usage: %s portfoliofile' % args[0])
+    filename = args[1]
+    print('Total cost:', portfolio_cost(filename))
+    
+    if __name__ == '__main__':
+    import sys
+    main(sys.argv)
