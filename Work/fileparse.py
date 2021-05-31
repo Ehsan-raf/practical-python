4467d6ae18874cb1fbcbf12a4e44dce053a80e12
Ehsan Rafieiazad <erafieiazad@mun.ca>
2021-05-30 21:49:23 -0230

    3_10

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,15 +1,15 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     
     if select and not has_headers:
-        raise RuntimeError("select argument requires column headers")
+        raise RuntimeError('select requires column headers')
         
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
-        headers = next(rows)
+        headers = next(rows) if has_headers else []
 
         if select:
             indices = [headers.index(colname) for colname in select]
@@ -19,12 +19,18 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
 
         records = []
         
-        for row in rows:
+        for rowno, row in enumerate(rows, 1):
             if not row: 
                 continue
                 
-            if indices:
-                row = [ row[index] for index in indices ]
+            if select:
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if not silence_errors:
+                        print(f"Row {rowno}: Couldn't convert {row}")
+                        print(f"Row {rowno}: Reason {e}")
+                    continue
 
             if types:
                 row = [func(val) for func, val in zip(types, row)]
@@ -32,7 +38,8 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             if headers:
                 record = dict(zip(headers, row))
                 
-            record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
-    return records
+        return records
